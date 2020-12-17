3a0c027833a4e4865d12f92155ff83c4fa661690
dimkob8139 <dimkob8139@gmail.com>
2020-12-17 10:47:28 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,68 @@
+from coverage import data
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from cloudipsp import Api, Checkout, Payment, checkout
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    isActive = db.Column(db.Boolean, default=True)
+    text = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return self.title
+
+
+@app.route('/buy/<int:id>')
+def item_buy(id):
+    item = Item.query.get(id)
+    api = Api(merchant_id=1396424,
+              secret_key='test')
+    checkout = Checkout(api=api)
+    data = {
+        "currency": "USD",
+        "amount": str(item.price) + '00'
+    }
+    url = checkout.url(data).get('checkout_url')
+    return redirect(url)
+
+
+@app.route('/')
+def index():
+    items = Item.query.order_by(Item.price).all()
+    return render_template('index.html', data=items)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    if request.method == "POST":
+        title = request.form['title']
+        price = request.form['price']
+        text = request.form['text']
+
+        item = Item(title=title, price=price, text=text)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Ошибка при обработке"
+    else:
+        return render_template('create.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
